18f8b05fb11aa220af45eef1c3d4db95bc1815de
MelihTakyaci <61650345+MelihTakyaci@users.noreply.github.com>
2023-12-21 17:17:05 +0300

    update 1.0
    First adam akilli update

--- graph.js
+++ graph.js
@@ -1,17 +1,20 @@
 var myChart;
-// X ve y degerlerini olustur y degerlerini calculateFunction a esitle
-function generateData(inputFunction) {
-    var x = [];
-    var y = [];
 
+function generateData(inputFunction1, inputFunction2 ,inputFunction3) {
+    var x = [];
+    var y1 = [];
+    var y2 = [];
+    var y3 = [];
     for (var i = -10; i <= 10; i += 0.1) {
         x.push(i);
-        y.push(calculateFunction(inputFunction, i));
+        y1.push(calculateFunction(inputFunction1, i));
+        y2.push(calculateFunction(inputFunction2, i));
+        y3.push(calculateFunction(inputFunction3, i));
     }
 
-    return { x: x, y: y };
+    return { x: x, y1: y1, y2: y2 , y3: y3 };
 }
-// math fonksiyonu ile trigonometrik fonksiyon olustur
+
 function calculateFunction(inputFunction, value) {
     try {
         var editedFunction = inputFunction
@@ -38,22 +41,26 @@ function generateGraph() {
         myChart.destroy();
     }
 
-    var inputFunction = document.getElementById('inputFunction').value;
-    var data = generateData(inputFunction);
+    var inputFunction1 = document.getElementById('inputFunction1').value;
+    var inputFunction2 = document.getElementById('inputFunction2').value;
+    var inputFunction3 = document.getElementById('inputFunction3').value;
 
-    // Y ekseninin minimum ve maksimum belirle
-    var minY = Math.min(...data.y);
-    var maxY = Math.max(...data.y);
+    // Sadece bir fonksiyon girildiyse ikinci fonksiyonu kullanma
+    if (!inputFunction2) {
+        inputFunction2 = inputFunction1;
+    }
+    if (!inputFunction3) {
+        inputFunction3 = inputFunction2;
+    }
+    var data = generateData(inputFunction1, inputFunction2 , inputFunction3);
 
-    // Maksimum ve minimum değerlerin 
+    var minY = Math.min(...data.y1, ...data.y2 , ...data.y3);
+    var maxY = Math.max(...data.y1, ...data.y2 , ...data.y3);
     var absMax = Math.abs(maxY);
     var absMin = Math.abs(minY);
-
-    // Mutlak degere gore max min bul
     var newMax = absMax > absMin ? -absMax : absMax;
     var newMin = absMax > absMin ? absMax : -absMin;
 
-    // X ekseninin min ve max degeri
     var minX = Math.min(...data.x);
     var maxX = Math.max(...data.x);
 
@@ -62,13 +69,29 @@ function generateGraph() {
         type: 'line',
         data: {
             labels: data.x,
-            datasets: [{
-                label: 'Function Graph',
-                data: data.y,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 2,
-                fill: false
-            }]
+            datasets: [
+                {
+                    label: 'Function Graph 1',
+                    data: data.y1,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 2,
+                    fill: false
+                },
+                {
+                    label: 'Function Graph 2',
+                    data: data.y2,
+                    borderColor: 'rgba(255, 0, 0, 1)',
+                    borderWidth: 2,
+                    fill: false
+                },
+                {
+                    label: 'Function Graph 3',
+                    data: data.y2,
+                    borderColor: 'rgba(255, 0, 0, 1)',
+                    borderWidth: 2,
+                    fill: false
+                },
+            ]
         },
         options: {
             scales: {
@@ -92,5 +115,4 @@ function generateGraph() {
                 }
             },
         }
-    });
-}
+    });}
